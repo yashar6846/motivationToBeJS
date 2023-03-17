87288d7b233b9e080eb52563a05ab64ca5a150ce
yashar <yasharqashqai@192.168.1.33>
2023-03-17 18:39:33 +0300

    add formreact

---
+++ react/fetchApi/FormReact.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+
+export default function Forming() {
+  const [form, setForm] = useState({ isim: "", sehir: "", bio: "" });
+  console.log("FORM", form);
+  const handleChange = event =>
+    setForm({ ...form, [event.target.name]: event.target.value });
+  return (
+    <div className="">
+      <h1>React Dersleri</h1>
+      <form>
+        <input
+          value={form.isim}
+          onChange={handleChange}
+          placeholder="adiniz"
+          name="isim"
+        />
+        <select value={form.sehir} onChange={handleChange} name="sehir">
+          <option value="" disabled>
+            Şehir seçiniz
+          </option>
+          <option value="ankara">Ankara</option>
+          <option value="istanbul">İstanbul</option>
+          <option value="izmir">İzmir</option>
+        </select>
+        <textarea
+          value={form.bio}
+          onChange={handleChange}
+          rows="10"
+          placeholder="biyografiniz"
+          name="bio"
+        />
+        <button>Gönder</button>
+      </form>
+     
+    </div>
+  );
+}
