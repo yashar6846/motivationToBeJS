e3e3f37c3a3f25c64a8345d6fd76a3ddecfa0a57
yashar <yasharqashqai@192.168.1.33>
2023-04-04 12:32:05 +0300

    add progect delete

--- react/JsonServer/jesonserver/my-json/src/components/pages/contact/Contact.jsx
+++ react/JsonServer/jesonserver/my-json/src/components/pages/contact/Contact.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react"
+import {useHistory} from "react-router"
 export const Contact=()=>{
+ const[name,setName]=useState()
+ const[imageurl,setImageurl]=useState()
+ const[author,setAuthor]=useState()
+ const[ispandings,setIspandings]=useState(false)
+//  const history = useHistory()
+
+ const handleSubmit =(e)=>{
+    e.preventDefault()
+    setIspandings(true)
+    const blog ={name, imageurl, author}
+    fetch(`http://localhost:2080/posts`, {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(blog)
+    }).then(()=>{
+        console.log('new post received');
+        // history.push('/')
+    })
+ }
     return(
         <>
-        <h1>Contact</h1>
+        <h3>Add a new Blog</h3>
+        <form onSubmit={handleSubmit}>
+        <label>Blog Title:</label>
+                <input 
+                type="text" 
+                required
+                value = {name}
+                onChange = {(e) => setName(e.target.value)}
+                />
+                <br />
+               <div>
+               <input 
+                type="text" 
+                required
+                value = {imageurl}
+                onChange = {(e) => setImageurl(e.target.value)}
+                />
+                </div> 
+            {ispandings&& <button>Submit</button>}
+            {!ispandings&& <button>is nat Submit</button>}
+        </form>        
         </>
     )
 }
+
--- react/JsonServer/jesonserver/my-json/src/components/pages/home/Artikle.jsx
+++ react/JsonServer/jesonserver/my-json/src/components/pages/home/Artikle.jsx
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom"
+import {useHistory,  useParams } from "react-router-dom"
 import { useFatch } from "../home/useFatch"
+import axios from "axios"
 
 export const Artikl=()=>{
   const param = useParams()
   const{data:blog,isPending,error}=useFatch(`http://localhost:2080/posts/${param.id}`)
+//   let history = useHistory();
+  const handeleDelete=()=>{
+    axios(`http://localhost:2080/posts/${blog.id}`,{
+        method:"DELETE",
+    }).then(()=>{
+        // history.push("/home");
+    })
+  }
     return(
         <>
         <h2>aboute</h2>
@@ -15,6 +24,7 @@ export const Artikl=()=>{
                 <div><img src={blog.imageurl}/></div>
                 <div>{blog.price}</div>
                  <p>{blog.body}</p>
+                 <button onClick={handeleDelete}>Delete</button>
             </article>
         )}
         </>
