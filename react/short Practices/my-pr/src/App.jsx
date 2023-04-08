0c7abdc07f983c8529ba14025d91207966b13039
yashar <yasharqashqai@192.168.1.33>
2023-04-08 17:29:52 +0300

    add practice small project

---
+++ react/short Practices/my-pr/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Weather } from './compoWeather/Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/short Practices/my-pr/src/compoWeather/Weather.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import axios from "axios";
+// import styles from 'weather.mudle.css'
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "75c10f679078c5f263f4657b9e74798e";
+
+export const Weather=()=>{
+ const[weather,setWeather]=useState({})
+ const[city,setCity]=useState('')
+
+ const getWeather= async (name)=>{
+    const{data}= await axios.get(URL,{
+      params:{
+        q:name,
+        units:"meterc",
+        APPID: API_KEY
+      }  
+    })
+    setWeather(data)
+ }
+ console.log(weather);
+ const handelSubmite=()=>{
+ 
+ }
+    return(
+        <>
+        <h3>Weather</h3>
+        <input type="text" onChange={(text)=> setCity(text.target.value)} />
+        <button onClick={()=>  getWeather(city)}>search</button>
+        {weather.main ? (
+          <div>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+              className="weatherIcon"
+            />
+            <div className="name">{weather.weather[0].main}</div>
+          </div>
+        ) : null}
+        </>
+    )
+}
