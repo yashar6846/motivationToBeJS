286f51ee98503978356ca3aaea70ab6b6277b6e0
yashar <yasharqashqai@192.168.1.33>
2023-04-22 20:49:15 +0300

    add updateRemove

--- react/shapingcart/sh-ca/src/components/Cart/Cart.jsx
+++ react/shapingcart/sh-ca/src/components/Cart/Cart.jsx
@@ -8,9 +8,24 @@ const Cart = (props) => {
    const cartCtx= useContext(CartContext)
 
    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+   const hasItems = cartCtx.items.length > 0
+
+   const cartITEMRemove =(id)=>{
+    cartCtx.removeItem(id)
+   }
+   const cartItemAddHandler = (item)=>{
+    cartCtx.addItem({...item, amount:1})
+   }
     const cartItems = <ul className={classes['cart-items']}>
         {cartCtx.items.map((item)=> 
-        <li>{item.name}</li>)}
+        <CartItem 
+        key={item.id}
+        name={item.name}
+        amount={item.amount}
+        price={item.price}
+        onRemove={cartITEMRemove.bind(null,item.id)}
+        onAdd={cartItemAddHandler.bind(null, item)}
+        />)}
     </ul>
     return (
         
@@ -22,7 +37,7 @@ const Cart = (props) => {
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-                <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );
